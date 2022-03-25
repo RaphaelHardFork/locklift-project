e62ad2a285580d797e238cdcb8a9baa4f1a7e24b
RaphaelHardFork <raphael.pellet@protonmail.com>
2022-03-25 12:06:56 +0100

    creation

---
+++ README.md
@@ -0,0 +1,38 @@
+# Venom contracts
+
+_[Locklift](https://github.com/broxus/ton-locklift) project from a proposed [template](https://github.com/EverscaleGuild/locklift-sample)_
+
+## Requirements
+
+- [yarn](https://yarnpkg.com/getting-started/install)
+- [docker](https://www.docker.com/get-started)
+- [everdev](https://github.com/tonlabs/everdev#installation)
+
+## Commandes
+
+```shell
+yarn compile
+yarn test
+```
+
+Change compiler version:
+
+```json
+// package.json
+"everdev-setup": "everdev sol set --compiler 0.58.2 --linker 0.14.39",
+```
+
+## TVM ressources
+
+Exemple of smart contracts written for TVM:
+
+- https://github.com/broxus/ton-eth-bridge-token-contracts
+- https://github.com/tonlabs/samples/tree/master/solidity
+
+Solidity API: https://github.com/tonlabs/TON-Solidity-Compiler/blob/master/API.md
+
+TVM description: https://test.ton.org/tvm.pdf
+
+Solidity compiler (last 0.58.2): https://github.com/tonlabs/TON-Solidity-Compiler
+
+VSCode extension for TON-Solidity: https://marketplace.visualstudio.com/items?itemName=everscale.solidity-support
---
+++ test/token-test.js
@@ -0,0 +1,78 @@
+/* eslint-disable mocha-no-only/mocha-no-only */
+/* eslint-disable camelcase */
+const { expect } = require('chai')
+
+let Token, token, keyPair, keyPair2, Wallet, wallet
+
+describe('Token contract', async function () {
+  describe('Contracts', async function () {
+    beforeEach(async function () {
+      ;[keyPair, keyPair2] = await locklift.keys.getKeyPairs()
+
+      Wallet = await locklift.factory.getContract('Wallet')
+      Token = await locklift.factory.getContract('Root')
+      token = await locklift.giver.deployContract({
+        contract: Token,
+        initParams: {
+          wallet_code: Wallet.code,
+        },
+        keyPair,
+      })
+    })
+
+    it('Load contract factory & deploy contract', async function () {
+      expect(Token.code).not.to.equal(undefined, 'Code should be available')
+      expect(Token.abi).not.to.equal(undefined, 'ABI should be available')
+
+      expect(token.address)
+        .to.be.a('string')
+        .and.satisfy((s) => s.startsWith('0:'), 'Bad future address')
+
+      console.log((await locklift.ton.getBalance(token.address)).toString())
+
+      // -------- deployEmptyWallet
+      // try {
+      //   await token.run({
+      //     method: 'deployEmptyWallet',
+      //     params: {
+      //       answerId: '10',
+      //       _wallet_public_key: `0x${keyPair2.public}`,
+      //       _deploy_evers: '100000000',
+      //     },
+      //     keyPair: keyPair,
+      //   })
+      // } catch (e) {
+      //   console.log(e)
+      // }
+      // ---
+
+      const startBalance = await token.call({ method: 'start_gas_balance' })
+      console.log(startBalance.toString())
+      // ------------------ mint
+      try {
+        await token.run({
+          method: 'mint',
+          params: { _to: keyPair2.public, _tokens: '10' },
+          keyPair: keyPair,
+          value: '100000',
+        })
+      } catch (e) {
+        console.log(e)
+      }
+      // ---
+    })
+
+    it('should mint token', async function () {
+      this.timeout(20000)
+    })
+
+    // it('Interact with contract', async function () {
+    //   const response = await sample.call({
+    //     method: 'getDetails',
+    //     params: {},
+    //   })
+
+    //   expect(response.toNumber()).to.be.equal(111, 'Wrong state')
+    // })
+  })
+})
